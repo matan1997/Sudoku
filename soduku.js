850d2d163a52426fe409397c26826a6bba838236
matan1997 <64330466+matan1997@users.noreply.github.com>
2023-06-01 13:37:57 +0300

    Update soduku.js
    delete logs

--- soduku.js
+++ soduku.js
@@ -241,7 +241,6 @@ const stopTimer = async() => {
 }
 
 const updateTimer = () => {
-    console.log(`new timer add $(timerStart)`);
     if(timerStart){
         const elapsedTime = Date.now() - startTime;
         const formattedTime = formatTime(elapsedTime);
